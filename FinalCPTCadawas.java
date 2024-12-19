9bac42aaef5d2fff5f0deb146013e43e30f80fec
Alfred Cadawas <alfred.cadawas@gmail.com>
2024-12-19 08:50:00 -0500

    Added task functionality

--- FinalCPTCadawas.java
+++ FinalCPTCadawas.java
@@ -6,6 +6,13 @@ public class FinalCPTCadawas{
 		con.println("Christmas is next week");
 		con.println("Last day of school!!!!");
 		con.println("Wait not... tomorrow is the last day of school!!!");
+		String strTask;
+		con.println("What will be your most important christmas task?");
+		strTask = con.readLine();
+		
+		TextOutputFile taskfile = new TextOutputFile("xmastask.txt", true);
+		taskfile.println(strTask);
+		taskfile.close();
 
 	}
 }
