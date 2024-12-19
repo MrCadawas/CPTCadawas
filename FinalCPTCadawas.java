a8b2781a3136b67f89942d02a236913adeb353e8
Alfred Cadawas <alfred.cadawas@gmail.com>
2024-12-19 08:45:20 -0500

    Created first few lines of code to test

---
+++ FinalCPTCadawas.java
@@ -0,0 +1,10 @@
+import arc.*;
+
+public class FinalCPTCadawas{
+	public static void main(String[] args){
+		Console con = new Console();
+		con.println("Last day of school!!!!");
+		con.println("Wait not... tomorrow is the last day of school!!!");
+
+	}
+}
