f981ba94347b516deb5cb6039aae96062b78b8b7
Alfred Cadawas <alfred.cadawas@gmail.com>
2024-12-19 08:46:58 -0500

    Added print about christmas

--- FinalCPTCadawas.java
+++ FinalCPTCadawas.java
@@ -3,6 +3,7 @@ import arc.*;
 public class FinalCPTCadawas{
 	public static void main(String[] args){
 		Console con = new Console();
+		con.println("Christmas is next week");
 		con.println("Last day of school!!!!");
 		con.println("Wait not... tomorrow is the last day of school!!!");
 
